8d6b0642fe6e146daf91015842d079b54c152c02
elgraney <elgraney501@gmail.com>
2018-04-20 18:21:16 +0100

    something changed presumably

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-
+        Planet planet = new PlanetCharacter().makePlanetCharacter(150000, 'f', 1);
     }
 }
