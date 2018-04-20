adb044366e93be5be2ab85c50ecc0ee78e94033e
elgraney <elgraney501@gmail.com>
2018-04-20 18:04:03 +0100

    oh wait no, stuff to do

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,10 @@
 package com.company;
 
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	Random rand = new Random();
-	System.out.println(((int)(rand.nextDouble()*100)/100.0)+0.5);
+
+
     }
 }
